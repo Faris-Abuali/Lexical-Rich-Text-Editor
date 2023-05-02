35e671e9101cefb0cbea919fd0b6860245035e9c
Faris Abuali <faris.abuali@outlook.com>
2023-05-02 04:39:36 +0300

    add deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  base: "/Lexical-Rich-Text-Editor/"
 })
